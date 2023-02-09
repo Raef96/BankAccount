542d42e7a3d887f51cbce4197b7340a38e6266d6
Raef <raef.oueslati0208@gmail.com>
2023-02-09 12:42:32 +0100

    Add DTOs to the domain layer

---
+++ src/Bank.Domain/Dtos/AccountDto.cs
@@ -0,0 +1,23 @@
+﻿using Bank.Domain.Entities;
+
+namespace Bank.Domain.Dtos;
+
+public class AccountDto
+{
+    public string Owner { get; set; }
+    public string IBAN { get; set; }
+    public string RIB { get; set; }
+    public decimal Balance { get; set; }
+
+    public AccountDto()
+    {
+    }
+
+    public AccountDto(Account account)
+    {
+        Owner = account.Owner;
+        IBAN = account.IBAN;
+        RIB = account.RIB;
+        Balance = account.Balance;
+    }
+}
---
+++ src/Bank.Domain/Dtos/TransactionDto.cs
@@ -0,0 +1,31 @@
+﻿using Bank.Domain.Enums;
+using Bank.Domain.Entities;
+
+namespace Bank.Domain.Dtos;
+
+public class TransactionDto
+{
+    public TransactionType TransactionType { get; set; }
+
+    public string OperationType => TransactionType.ToString();
+
+    public string Currency { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public TransactionStatus Status { get; set; }
+
+    public TransactionDto()
+    {
+    }
+    public TransactionDto(Transaction transaction)
+    {
+        TransactionType = transaction.Type;
+        Currency = transaction.Currency;
+        Amount = transaction.Amount;
+        Status = transaction.Status;
+        CreationDate = transaction.CreationDate;
+    }
+}
