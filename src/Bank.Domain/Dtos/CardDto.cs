cb7618abefa4b459cdf9c2fb14dae1f148dd5b46
Raef <raef.oueslati0208@gmail.com>
2023-02-10 18:29:37 +0100

    Add CardDto to the domain

---
+++ src/Bank.Domain/Dtos/CardDto.cs
@@ -0,0 +1,12 @@
+﻿using Bank.Domain.Enums;
+
+namespace Bank.Domain.Dtos;
+
+public class CardDto
+{
+    public Guid AccountId { get; set; }
+    public string Owner { get; set; }
+    public string CardNumber { get; set; }
+    public CardStatus CardStatus { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
