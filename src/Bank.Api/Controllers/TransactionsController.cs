31fd46170a9f420b31751ca9c8348671290c617a
Raef <raef.oueslati0208@gmail.com>
2023-02-12 20:18:53 +0100

    Create Transactions controller

---
+++ src/Bank.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,29 @@
+﻿using Bank.Domain.Dtos;
+using Bank.App.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionsController : ControllerBase
+{
+    private readonly ITransactionService _transactionService;
+
+    public TransactionsController(ITransactionService transactionService)
+    {
+        _transactionService = transactionService;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<TransactionDto>> Get()
+    {
+        return Ok(_transactionService.GetAllTransactions());
+    }
+
+    [HttpGet("/{accountId}")]
+    public ActionResult<IEnumerable<TransactionDto>> GetByAccountId(Guid accountId)
+    {
+        return Ok(_transactionService.GetByAccountId(accountId));
+    }
+}
