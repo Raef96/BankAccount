e8a6f803f735cd4662d960042e6af776e97097ec
Raef <raef.oueslati0208@gmail.com>
2023-02-12 20:18:18 +0100

    Set up the services and use of In memory database for the Bank Api

--- src/Bank.Api/Program.cs
+++ src/Bank.Api/Program.cs
@@ -1,6 +1,23 @@
+using Bank.Infrastructure.Extensions;
+using Bank.Infrastructure.Persistance;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+/*----- Add bank Services----/
+ * ---------------------*/
+builder.Services.AddBankServices();
+
+
+/*----In Memory sqlLite Databse for developement ----*/
+if (builder.Environment.IsDevelopment())
+{
+    var connection = new SqliteConnection("DataSource=:memory:");
+    connection.Open();
+    builder.Services.AddDbContext<BankDbContext>(opts =>
+        opts.UseSqlite(connection));
+}
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
