bdce3b2e9be5b4b29c19ab98bce5395e6d5bb287
Raef <raef.oueslati0208@gmail.com>
2023-02-12 20:16:57 +0100

    Create Services Extension

--- src/Bank.Infrastructure/Extensions/ServicesExtension.cs
+++ src/Bank.Infrastructure/Extensions/ServicesExtension.cs
@@ -1,5 +1,34 @@
-﻿namespace Bank.Infrastructure.Extensions;
+﻿using Bank.App.Interfaces.Services;
+using Bank.App.Interfaces.Repositories;
+using Bank.Infrastructure.Persistance;
+using Bank.Infrastructure.ServicesImpl;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bank.Infrastructure.Extensions;
 
 internal static class ServicesExtension
 {
+    public static IServiceCollection AddBankServices(this IServiceCollection services)
+    {
+        return services.AddRepositories()
+                       .AddServices();
+
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<IAccountRepository, AccountRepository>();
+        services.AddTransient<ITransactionRepository, TransactionRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+
+        services.AddTransient<IAccountService, AccountService>();
+        services.AddTransient<ITransactionService, TransactionService>();
+
+        return services;
+    }
 }
